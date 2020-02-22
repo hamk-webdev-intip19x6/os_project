47349caf905fc43c39dcc3266dd16391e0e8d26b
Ubuntu <JH@VM2020.vhuvnn53ztwuzbfpxzt0brvp4a.fx.internal.cloudapp.net>
2020-02-22 00:16:28 +0000

    garbage

--- mysite/rental_system/views.py
+++ mysite/rental_system/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
-
+from django.db.models import Q
+from operator import attrgetter
 from .models import Work, Type, RentedWork
+
 
 def index(request):
     works = Work.objects.all()
     types = Type.objects.all()
     context = {'works': works, 'types': types}
+
+    #search
+    query = ""
+    if request.GET:
+        query = request.GET['search']
+        context['query'] = str(query)
+    
+    posts = sorted(search(query), key=attrgetter('title'), reverse=True)
+    context['posts'] = posts
+    
+
     return render(request, 'rental_system/index.html', context)
 
 def work(request, work_id):
@@ -20,3 +33,14 @@ def rented(request):
     rented = list(RentedWork.objects.filter(user_id = current_user.id))
     
     return render(request, 'rental_system/rented.html', {'rented': rented})
+
+def search(query=None):
+    queryset = []
+    queries = query.split(" ") #
+    for search in queries:
+        #obj = Work.objects.filter(Q(title__icontains=search) | Q(body__icontains=search))
+        obj = Work.objects.filter(Q(title__iexact=search) | Q(desc__iexact=search))
+
+        for post in obj:
+            queryset.append(post)
+    return list(set(queryset))
