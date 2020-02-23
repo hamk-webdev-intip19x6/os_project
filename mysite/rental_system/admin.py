edde2a69898abd81befd6ce226df22aada65448c
Folnug <49916728+folnug@users.noreply.github.com>
2020-02-23 03:04:33 +0200

    Admin parannuksia

--- mysite/rental_system/admin.py
+++ mysite/rental_system/admin.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Creator, Type, Work, Genre, Publisher, RentedWork
+
+class RentedWorkAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rented_work', 'rent_date', 'return_date',)
+    search_fields = ['rented_work__title']
+    list_filter = ['rented_work__type']
+
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'desc', 'type', 'pub_date',)
+    list_filter = ['pub_date', 'type', 'publishers__publisher', 'genres__genre']
+    search_fields = ['title']
+
+admin.site.register(RentedWork, RentedWorkAdmin)
+admin.site.register(Work, WorkAdmin)
+
 admin.site.register(Creator)
 admin.site.register(Type)
-admin.site.register(Work)
 admin.site.register(Genre)
 admin.site.register(Publisher)
-admin.site.register(RentedWork)
