1de6a796e3d2588765e0f1150b789f1a1428d1f0
JH <joni1816@student.hamk.fi>
2020-02-29 16:37:31 +0000

    Formit fixattu

--- mysite/register/forms.py
+++ mysite/register/forms.py
@@ -4,14 +4,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
-    #would be smarter to use 'django_widget_tweaks' but this works
-    #force the basic forms to use bootstrap forms instead
-    email = forms.EmailField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Email"}))
-    first_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"First Name"}), required=False)
-    last_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Last Name"}), required=False)
-    username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Username"}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder":"Password"}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder":"Confirm Password"}))
+    ####legacy code, replaced with django-widget-tweaks####
+    #email = forms.EmailField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Email"}))
+    #first_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"First Name"}), required=False)
+    #last_name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Last Name"}), required=False)
+    #username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Username"}))
+    #password1 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder":"Password"}))
+    #password2 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder":"Confirm Password"}))
 
     class Meta:
         model = User
